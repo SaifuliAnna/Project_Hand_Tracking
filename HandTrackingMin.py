e299df925a8f5b1d23b26a8593d9a572ce9807f7
Saifulina Anna <saifulianna.it@gmail.com>
2023-12-06 23:41:52 +0200

    1_finish

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -5,8 +5,37 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+previous_time = 0
+current_time = 0
+
+
 while True:
     success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_rgb)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for hand_lms in results.multi_hand_landmarks:
+            for id, lm in enumerate(hand_lms.landmark):
+                # print(id, lm)
+                height, weight, chanel = img.shape
+                chanel_x, chanel_y = int(lm.x * weight), int(lm.y * height)
+                print(id, chanel_x, chanel_y)
+                if id == 4:
+                    cv2.circle(img, (chanel_x, chanel_y), 15, (255, 0, 255), cv2.FILLED)
+
+            mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
+
+    current_time = time.time()
+    fps = 1/(current_time - previous_time)
+    previous_time = current_time
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
