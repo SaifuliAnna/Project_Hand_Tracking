9d75bfea9713c88916622b5bb3da86f677014ea7
Saifulina Anna <saifulianna.it@gmail.com>
2023-12-09 12:05:35 +0200

    add comments

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -1,24 +1,33 @@
-import cv2
-import mediapipe as mp
+import cv2  # OpenCV is used to work with the video stream and image processing.
+import mediapipe as mp  # Mediapipe is used to detect and track key points on the hands.
 import time
 
-
+# Video camera initialization:
+# Create a VideoCapture object to read a video stream from a webcam (0 points to the first available camera).
 cap = cv2.VideoCapture(0)
 
+# Initializing Mediapipe for hand detection:
+# Creating objects for working with hands using Mediapipe
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_draw = mp.solutions.drawing_utils
 
+# Initializing variables for time tracking:
+# Variables for calculating frames per second (FPS).
 previous_time = 0
 current_time = 0
 
-
+# Endless loop for video stream processing:
 while True:
+    # A loop that endlessly reads frames from a webcam.
     success, img = cap.read()
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(img_rgb)
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Converts the BGR color space to RGB
+    results = hands.process(img_rgb)  # Call the process method to detect hands in an image.
     # print(results.multi_hand_landmarks)
 
+    # Processing hand detection results:
+    # Checks if hand detection results are available.
     if results.multi_hand_landmarks:
         for hand_lms in results.multi_hand_landmarks:
             for id, lm in enumerate(hand_lms.landmark):
@@ -26,16 +35,19 @@ while True:
                 height, weight, chanel = img.shape
                 chanel_x, chanel_y = int(lm.x * weight), int(lm.y * height)
                 print(id, chanel_x, chanel_y)
+                # If so, the index and coordinates of each key point are displayed for each hand.
+                # A circle is drawn on the first key point (with index 0) of the hand.
                 if id == 0:
                     cv2.circle(img, (chanel_x, chanel_y), 25, (255, 0, 255), cv2.FILLED)
-
             mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
-
+    # Calculate and display frames per second in the image.
     current_time = time.time()
     fps = 1/(current_time - previous_time)
     previous_time = current_time
 
     cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-
+    # Image display:
+    # Displays an image with tracked hands and other information.
+    # cv2.waitKey (1) - waits for pressing the delay key for correct operation of the display window.
     cv2.imshow("Image", img)
     cv2.waitKey(1)
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -3,12 +3,17 @@ import mediapipe as mp
 import time
 
 
-class HandDetector():
+class HandDetector:
     def __init__(self, mode=False, max_hands=2, model_complexity=1, detection_confidence=0.5, tracking_confidence=0.5):
-        self.mode = mode
-        self.max_hands = max_hands
+        self.mode = mode  # still image mode on (True) or off (False)
+        self.max_hands = max_hands  # maximum number of hands a model can detect in one frame
+        # complexity of the model. This is usually a number from 0 to 2, where 0 - smallest complexity and 2 - largest
         self.model_complexity = model_complexity
+        #  Specifies the trust threshold for hand detection.
+        #  Hands that are detected with less confidence can be discarded
         self.detection_confidence = detection_confidence
+        #  Specifies the trust threshold for hand tracking.
+        #  Hands that cannot be tracked with sufficient confidence may be lost.
         self.tracking_confidence = tracking_confidence
 
         self.mp_hands = mp.solutions.hands
@@ -17,6 +22,15 @@ class HandDetector():
         self.mp_draw = mp.solutions.drawing_utils
 
     def find_hands(self, img, draw=True):
+        """
+        - Converts the color format of a BGR image to RGB.
+        - Calls the process method for processing hands on an image and stores the results
+          in the results attribute.
+        - If draw is set to True, the method also draws key points and the lines connecting them.
+        :param img:
+        :param draw:
+        :return: img
+        """
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(img_rgb)
         # print(results.multi_hand_landmarks)
@@ -29,6 +43,16 @@ class HandDetector():
         return img
 
     def find_position(self, img, hand_number=0, draw=True):
+        """
+        - Returns a list of coordinates of the key points of the hand (lm_list).
+        - Each point is represented as [id, chanel_x, chanel_y], where id is the index of the point,
+          chanel_x and chanel_y are the coordinates of the point in the image.
+        - If draw is set to True, then the method also draws circles in the image at key point locations.
+        :param img:
+        :param hand_number:
+        :param draw:
+        :return: lm_list
+        """
         lm_list = []
 
         if self.results.multi_hand_landmarks:
@@ -42,6 +66,7 @@ class HandDetector():
                     cv2.circle(img, (chanel_x, chanel_y), 15, (255, 0, 255), cv2.FILLED)
 
         return lm_list
+
 
 def main():
     cap = cv2.VideoCapture(0)
